f46b540538d09aeaa90b220b44637dc38fdfc051
Lukman <engr.lukman@gmail.com>
2023-10-29 17:23:47 +0600

    Modify Home Page Text

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,18 @@
 <template>
   <q-page class="flex flex-center">
+    <h5>Home Page</h5>
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
