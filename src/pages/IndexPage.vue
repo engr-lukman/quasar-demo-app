f314e7610416a97407a3d660c1507d1a9de60232
Lukman <engr.lukman@gmail.com>
2023-10-29 17:31:38 +0600

    Modify Home Page Text

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,13 @@
 <template>
   <q-page class="flex flex-center">
     <h5>Home Page</h5>
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+    <div>
+      <img
+        alt="Quasar logo"
+        src="~assets/quasar-logo-vertical.svg"
+        style="width: 200px; height: 200px"
+      />
+    </div>
   </q-page>
 </template>
 
